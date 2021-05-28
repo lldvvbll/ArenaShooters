7159efbdc34dbe1aab61af591dafe4d44d4289cd
류현우 <lldvvbll@nate.com>
2021-05-29 00:04:54 +0900

    Initial commit

---
+++ Source/ArenaShooters/Public/ArenaShooters.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "EngineMinimal.h"
+#include "UObject/UObjectGlobals.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(ArenaShooters, Log, All);
+
+#define AS_LOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define AS_LOG_S(Verbosity) UE_LOG(ArenaShooters, Verbosity, TEXT("%s"), *AS_LOG_CALLINFO)
+#define AS_LOG(Verbosity, Format, ...) UE_LOG(ArenaShooters, Verbosity, TEXT("%s %s"), *AS_LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+//#define return_if(Expr, ...) { if (Expr) { LOG(Error, TEXT("ASSERTION: %s"), TEXT("'"#Expr"'")); return __VA_ARGS__; } }
---
+++ Source/ArenaShooters/Public/ArenaShootersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ArenaShooters.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArenaShootersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArenaShootersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArenaShootersGameMode();
+};
+
+
+
